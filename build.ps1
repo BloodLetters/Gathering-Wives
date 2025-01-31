3e82b03cad6d9d115f778b20517a0e4a05996130
BloodLetters <bloodletters45@gmail.com>
2025-01-31 20:15:32 +0800

    Changes to be committed: 	new file:   build.ps1 	new file:   ok-ww.exe 	modified:   setup.py

---
+++ build.ps1
@@ -0,0 +1,4 @@
+python -m ok.update.package_launcher "v1.0" deploy.txt
+Copy-Item -Path "dist" -Destination "ok-ww" -Recurse
+7z a -t7z -r "ok-ww-v1.0.7z" "ok-ww"     
+# Remove-Item -Path "ok-ww" -Recurse -Force
